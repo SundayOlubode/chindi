e14b84d86eedfe4d7fdc0215e99b90605b07e90c
Sam <s.olubode@alustudent.com>
2024-05-31 16:15:52 +0100

    chore: task menu

---
+++ lib/routes/task_menu.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TaskMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromRGBO(255, 193, 68, 0),
+      body: Padding(
+        padding: EdgeInsets.all((10)),
+      ),
+    );
+  }
+}
