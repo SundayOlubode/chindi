2716d4c6b7bef48566bc02b3e90ec43a5dec0d97
Sam <s.olubode@alustudent.com>
2024-05-31 16:56:58 +0100

    chore: add search bar

--- lib/routes/task_menu.dart
+++ lib/routes/task_menu.dart
@@ -3,11 +3,36 @@ import 'package:flutter/material.dart';
 class TaskMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromRGBO(255, 193, 68, 0),
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(255, 193, 68, 0),
       body: Padding(
-        padding: EdgeInsets.all((10)),
+        padding: const EdgeInsets.all((10)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(
+              height: 30.0,
+            ),
+            _buildSearchBar(context),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildSearchBar(BuildContext context) {
+    return SearchBar(
+      hintText: 'Search tasks...',
+      leading: IconButton(
+        onPressed: () => {},
+        icon: const Icon(Icons.search),
+      ),
+      trailing: [
+        IconButton(
+          onPressed: () => {},
+          icon: const Icon(Icons.mic),
+        ),
+      ],
     );
   }
 }
