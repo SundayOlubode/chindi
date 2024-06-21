cecbf3721d7fa94ee8f9190e3cf301e038e3b70e
Anesu Kafesu <a.kafesu@alustudent.com>
2024-06-21 08:07:20 +0200

    task

---
+++ lib/models/task.dart
@@ -0,0 +1,18 @@
+import 'package:chindi_ke/models/location.dart';
+import 'package:chindi_ke/models/user.dart';
+
+class Task {
+  final String title;
+  final double pay;
+  final String description;
+  final Location location;
+  final User owner;
+
+  const Task({
+    required this.title,
+    required this.pay,
+    required this.description,
+    required this.location,
+    required this.owner,
+  });
+}
