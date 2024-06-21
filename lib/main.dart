b65b1e1e15ddba9c0b17d946c0cb44ef51028dd1
Anesu Kafesu <a.kafesu@alustudent.com>
2024-06-21 08:52:17 +0200

    removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
-import 'package:chindi_ke/routes/auth/forgot_password.dart';
-import 'package:chindi_ke/routes/auth/login.dart';
-import 'package:chindi_ke/routes/auth/reset_password.dart';
-import 'package:chindi_ke/routes/auth/signin.dart';
-import 'package:chindi_ke/routes/auth/signup.dart';
-import 'package:chindi_ke/routes/chat.dart';
 import 'package:chindi_ke/routes/home.dart';
-import 'package:chindi_ke/routes/tasks/list_new_task.dart';
 import 'package:chindi_ke/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
-
-import 'routes/profile/user_profile.dart';
 
 void main() {
   runApp(const App());
