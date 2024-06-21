d37725f50711f5b86a9c4198f883dedf3ea18842
Anesu Kafesu <a.kafesu@alustudent.com>
2024-06-21 08:07:09 +0200

    location

---
+++ lib/models/location.dart
@@ -0,0 +1,15 @@
+class Location {
+  final String streetAddress;
+  final String suburb;
+  final String city;
+  final String county;
+  final String province;
+
+  const Location({
+    required this.streetAddress,
+    required this.suburb,
+    required this.city,
+    required this.county,
+    required this.province,
+  });
+}
