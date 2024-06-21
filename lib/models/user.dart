dd91b67ba706e187b8a69afb19516c339aa98627
Anesu Kafesu <a.kafesu@alustudent.com>
2024-06-21 08:14:35 +0200

    task

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,1 +1,17 @@
-class User {}
+import 'package:chindi_ke/models/location.dart';
+
+class User {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String uid;
+  final Location location;
+
+  const User({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.location,
+    required this.uid,
+  });
+}
