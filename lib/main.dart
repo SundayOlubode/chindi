fef0b9264555f9cfe08a3a3a853544ba68706d6b
NinaMwangi <n.mwangi@alustudent.com>
2024-06-24 15:25:08 +0300

    Task Fail Page

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,11 @@ import 'package:chindi_ke/routes/auth/reset_password.dart';
 import 'package:chindi_ke/routes/auth/signin.dart';
 import 'package:chindi_ke/routes/auth/signup.dart';
 import 'package:chindi_ke/routes/list_new_task.dart';
+import 'package:chindi_ke/routes/reg_unsuccesful.dart';
 import 'package:chindi_ke/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:chindi_ke/routes/reg_succesful.dart';
+import 'package:chindi_ke/routes/reg_unsuccesful.dart';
 
 import 'routes/profile/user_profile.dart';
 
@@ -23,7 +26,7 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: ChindiAppTheme.lightTheme,
       darkTheme: ChindiAppTheme.darkTheme,
-      home: const FormValidation(),
+      home: const RegFail(),
     );
   }
 }
---
+++ lib/routes/reg_unsuccesful.dart
@@ -0,0 +1,60 @@
+import 'package:awesome_ripple_animation/awesome_ripple_animation.dart';
+import 'package:flutter/material.dart';
+import 'package:chindi_ke/utils/constants/texts.dart';
+import 'package:chindi_ke/components/primary_button.dart';
+import '../../utils/constants/sizes.dart';
+
+class RegFail extends StatefulWidget {
+  const RegFail({super.key});
+
+  @override
+  State<RegFail> createState() => _RegFailState();
+}
+
+class _RegFailState extends State<RegFail> {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme globalTextTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ///TITLE TEXT
+              const Text(
+                'Task Creation Failed',
+                style: TextStyle(
+                  fontSize:
+                      ChindiSizes.fontSizeLg, // Use the size from sizes.dart
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: ChindiSizes.spaceBtwSections),
+            RippleAnimation(
+              size: const Size(200, 200),
+              repeat: false,
+              duration: const Duration(milliseconds: 2300),
+              ripplesCount: 8,
+              color: Colors.purple,
+              minRadius: 80,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.asset("assets/images/Fail.png", fit: BoxFit.cover, width: 100.0, height: 100,),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              ChindiTexts.registrationFail,
+              style: globalTextTheme.bodyLarge,
+              ),
+            const SizedBox(height: 20),
+            PrimaryButton(
+              onPressed: () {},
+              label: 'Retry',
+            ),
+          ]  
+        ),
+      ),
+    );
+  }
+}
