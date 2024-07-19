9f6edd87dfd55e56fc74329bbca59d0f4fa5c19a
Anesu Kafesu <a.kafesu@alustudent.com>
2024-07-19 13:06:04 +0200

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:chindi/routes/auth/sign_in.dart';
 import 'package:chindi/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:chindi/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
